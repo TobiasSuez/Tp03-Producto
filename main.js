a7d9a1ef605fcf47deb1ca8089f19356fdff14cd
TobiasSuez <116273023+TobiasSuez@users.noreply.github.com>
2023-04-19 09:01:16 -0300

    Funciona Barra de busqueda

--- main.js
+++ main.js
@@ -4,12 +4,32 @@ fetch('https://dummyjson.com/products')
     console.log("obtuve respuesta")
         valores = document.getElementById("producto");
         json.products.forEach(elemento => {
-            console.log(elemento.title)
             li=document.createElement("li")
-            li.innerHTML=`Nombre :${elemento.title}`
+            li.innerHTML=`Nombre: ${elemento.title}`
             valores.appendChild(li)
         });
   })    
     .catch(err => console.error("error", err))
     console.log("Fin consulta - fetch")
 
+//OTRA
+function Buscar(){
+  var el = document.getElementById("Producto").getElementsByTagName("li");
+  for (let index = 0; index < el.length; index++) {
+    valores.removeChild(li)
+  }
+  Ingresos= document.getElementById("Ingreso").value
+      fetch('https://dummyjson.com/products/search?q='+Ingresos)
+    .then(res => res.json())
+    .then(json =>{
+      console.log("obtuve respuesta")
+          valores = document.getElementById("producto");
+          json.products.forEach(elemento => {
+              li=document.createElement("li")
+              li.innerHTML=`Nombre: ${elemento.title}`
+              valores.appendChild(li)
+          });
+    })    
+      .catch(err => console.error("error", err))
+      console.log("Fin consulta - fetch")
+}

a4057b7262057dd801c107a47f51d7ad818fce42
TobiasSuez <116273023+TobiasSuez@users.noreply.github.com>
2023-04-24 10:44:12 -0300

    Funciona parcial

--- main.js
+++ main.js
@@ -3,9 +3,36 @@ fetch('https://dummyjson.com/products')
   .then(json => {
     valores = document.getElementById("producto");
     json.products.forEach(elemento => {
+      const Boton = document.createElement("button")
+      Boton.setAttribute("id", "miBoton")
+      Boton.innerHTML = "Mas info"
+      Contenido=document.getElementById("contenido")
+      Contenido.innerHTML= `Descripcion: ${elemento.description}`
+      
       li = document.createElement("li")
       li.innerHTML = `Nombre: ${elemento.title}`
       valores.appendChild(li)
+      valores.appendChild(Boton)
+
+      var modal = document.getElementById("myModal");
+
+      var btn = document.getElementById("miBoton");
+
+      var span = document.getElementsByClassName("close")[0];
+
+      btn.onclick = function () {
+        modal.style.display = "block";
+      }
+
+      span.onclick = function () {
+        modal.style.display = "none";
+      }
+
+      window.onclick = function (event) {
+        if (event.target == modal) {
+          modal.style.display = "none";
+        }
+      }
     });
   })
   .catch(err => console.error("error", err))
@@ -13,8 +40,12 @@ fetch('https://dummyjson.com/products')
 //OTRA
 function Buscar() {
   var el = document.getElementById("producto").getElementsByTagName("li");
+  var lol=document.getElementById("producto").getElementsByTagName("miBoton")
   for (let element of el) {
     element.classList.add("d-none");
+  }
+  for(let elementos of lol){
+    elementos.classList.add("d-none")
   }
   Ingresos = document.getElementById("Ingreso").value
   fetch('https://dummyjson.com/products/search?q=' + Ingresos)
@@ -30,20 +61,17 @@ function Buscar() {
     .catch(err => console.error("error", err))
 }
 
-function Categorias() {
-  const select=document.getElementById("Select")
-  select.addEventListener('change' , function handleChange(event){
-    console.log(event.target.value);
-    console.log(select.options[select.selectedIndex].value);
-    console.log(select.options[select.selectedIndex].text)
 
-  })
+const select = document.getElementById("Select")
+select.addEventListener('change', function handleChange(event) {
+  var categoria = select.options[select.selectedIndex].text
+
   var el = document.getElementById("producto").getElementsByTagName("li");
   for (let element of el) {
     element.classList.add("d-none");
   }
-  Ingresos = document.getElementById("Ingreso").value
-  fetch('https://dummyjson.com/products/category/' + Ingresos)
+
+  fetch('https://dummyjson.com/products/category/' + categoria)
     .then(res => res.json())
     .then(json => {
       valores = document.getElementById("producto");
@@ -54,5 +82,6 @@ function Categorias() {
       });
     })
     .catch(err => console.error("error", err))
+})
 
-}
+

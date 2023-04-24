981aa3cfb52da04edf717a766582a0aa8a16ec92
TobiasSuez <116273023+TobiasSuez@users.noreply.github.com>
2023-04-24 09:08:11 -0300

    casi funciona

--- main.js
+++ main.js
@@ -1,35 +1,58 @@
 fetch('https://dummyjson.com/products')
   .then(res => res.json())
-  .then(json =>{
-    console.log("obtuve respuesta")
-        valores = document.getElementById("producto");
-        json.products.forEach(elemento => {
-            li=document.createElement("li")
-            li.innerHTML=`Nombre: ${elemento.title}`
-            valores.appendChild(li)
-        });
-  })    
-    .catch(err => console.error("error", err))
-    console.log("Fin consulta - fetch")
+  .then(json => {
+    valores = document.getElementById("producto");
+    json.products.forEach(elemento => {
+      li = document.createElement("li")
+      li.innerHTML = `Nombre: ${elemento.title}`
+      valores.appendChild(li)
+    });
+  })
+  .catch(err => console.error("error", err))
 
 //OTRA
-function Buscar(){
-  var el = document.getElementById("Producto").getElementsByTagName("li");
-  for (let index = 0; index < el.length; index++) {
-    valores.removeChild(li)
+function Buscar() {
+  var el = document.getElementById("producto").getElementsByTagName("li");
+  for (let element of el) {
+    element.classList.add("d-none");
   }
-  Ingresos= document.getElementById("Ingreso").value
-      fetch('https://dummyjson.com/products/search?q='+Ingresos)
+  Ingresos = document.getElementById("Ingreso").value
+  fetch('https://dummyjson.com/products/search?q=' + Ingresos)
     .then(res => res.json())
-    .then(json =>{
-      console.log("obtuve respuesta")
-          valores = document.getElementById("producto");
-          json.products.forEach(elemento => {
-              li=document.createElement("li")
-              li.innerHTML=`Nombre: ${elemento.title}`
-              valores.appendChild(li)
-          });
-    })    
-      .catch(err => console.error("error", err))
-      console.log("Fin consulta - fetch")
+    .then(json => {
+      valores = document.getElementById("producto");
+      json.products.forEach(elemento => {
+        li = document.createElement("li")
+        li.innerHTML = `Nombre: ${elemento.title}`
+        valores.appendChild(li)
+      });
+    })
+    .catch(err => console.error("error", err))
+}
+
+function Categorias() {
+  const select=document.getElementById("Select")
+  select.addEventListener('change' , function handleChange(event){
+    console.log(event.target.value);
+    console.log(select.options[select.selectedIndex].value);
+    console.log(select.options[select.selectedIndex].text)
+
+  })
+  var el = document.getElementById("producto").getElementsByTagName("li");
+  for (let element of el) {
+    element.classList.add("d-none");
+  }
+  Ingresos = document.getElementById("Ingreso").value
+  fetch('https://dummyjson.com/products/category/' + Ingresos)
+    .then(res => res.json())
+    .then(json => {
+      valores = document.getElementById("producto");
+      json.products.forEach(elemento => {
+        li = document.createElement("li")
+        li.innerHTML = `Nombre: ${elemento.title}`
+        valores.appendChild(li)
+      });
+    })
+    .catch(err => console.error("error", err))
+
 }

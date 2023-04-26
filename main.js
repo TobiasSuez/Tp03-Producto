3230c8337bab802ca17f718fd390df64537b3e9a
TobiasSuez <116273023+TobiasSuez@users.noreply.github.com>
2023-04-26 09:15:39 -0300

    V2

--- main.js
+++ main.js
@@ -3,47 +3,32 @@ fetch('https://dummyjson.com/products')
   .then(json => {
     valores = document.getElementById("producto");
     json.products.forEach(elemento => {
-      const Boton = document.createElement("button")
-      Boton.setAttribute("id", "miBoton")
-      Boton.innerHTML = "Mas info"
-      Contenido=document.getElementById("contenido")
-      Contenido.innerHTML= `Descripcion: ${elemento.description}`
-      
+
       li = document.createElement("li")
-      li.innerHTML = `Nombre: ${elemento.title}`
+      li.innerHTML = `
+      <div>
+      <h5>Nombre: ${elemento.title}</h5>
+      <button onclick="cargarValores('${elemento.title}','${elemento.thumbnail}','${elemento.description}','${elemento.price}')">Mas Info</button>
+      </div>`
       valores.appendChild(li)
-      valores.appendChild(Boton)
 
-      var modal = document.getElementById("myModal");
-
-      var btn = document.getElementById("miBoton");
-
-      var span = document.getElementsByClassName("close")[0];
-
-      btn.onclick = function () {
-        modal.style.display = "block";
-      }
-
-      span.onclick = function () {
-        modal.style.display = "none";
-      }
-
-      window.onclick = function (event) {
-        if (event.target == modal) {
-          modal.style.display = "none";
-        }
-      }
     });
   })
   .catch(err => console.error("error", err))
+  
+function cargarValores(titulo, imagen, descripcion, precio){
 
+    document.getElementById("titulo").innerHTML = `${titulo}`
+    document.getElementById("imagen").src = `${imagen}`
+    document.getElementById("descripcion").innerHTML = `${descripcion}`
+    document.getElementById("precio").innerHTML= `$${precio}`
+
+}
 //OTRA
 function Buscar() {
   var el = document.getElementById("producto").getElementsByTagName("li");
-  var lol=document.getElementById("producto").getElementsByTagName("miBoton")
   for (let element of el) {
     element.classList.add("d-none");
-    lol.classList.add("d-none")
   }
 
   Ingresos = document.getElementById("Ingreso").value
@@ -64,12 +49,9 @@ function Buscar() {
 const select = document.getElementById("Select")
 select.addEventListener('change', function handleChange(event) {
   var categoria = select.options[select.selectedIndex].text
-
   var el = document.getElementById("producto").getElementsByTagName("li");
   for (let element of el) {
     element.classList.add("d-none");
-    lol.classList.add("d-none")
-
   }
 
   fetch('https://dummyjson.com/products/category/' + categoria)
@@ -84,5 +66,4 @@ select.addEventListener('change', function handleChange(event) {
     })
     .catch(err => console.error("error", err))
 })
-
 
